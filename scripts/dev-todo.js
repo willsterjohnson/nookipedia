e1fbf8a1c780e0c5680753ca789db5a3c47b16e8
Will Johnson <85991558+willster277@users.noreply.github.com>
2022-02-17 18:40:11 +0000

    docs: add documentation

--- scripts/dev-todo.js
+++ scripts/dev-todo.js
@@ -1,4 +1,7 @@
 /**
+ * Remove comments marked `// TODO` or `* @dev`.
+ *
+ * Does not remove comments marked `* @todo` as the info should be helpful to the user.
  * @param {string} f
  */
 export default (f) => f.replace(/\s*?(\*|\/\/)\s*?(@dev|TODO).+?\n/g, "\n");
--- scripts/fix-this-please.js
+++ scripts/fix-this-please.js
@@ -1,4 +1,7 @@
 /**
+ * This shouldn't be necessary, but here we are.
+ *
+ * Please submit a PR if you know how to fix it :)
  * @param {string} f
  */
 export default (f) => f.replace('/// <reference types="package/types" />', '/// <reference types="./types" />');
